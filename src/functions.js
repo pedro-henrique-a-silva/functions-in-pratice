83aaebbcb22d4661a147562872373ce41268f8eb
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-08 15:29:33 -0300

    Requisito 2 Criação da função circleArea

--- src/functions.js
+++ src/functions.js
@@ -8,11 +8,14 @@ function ligarDesligar(estado) {
   console.log('O motor está ligado');
   return 'ligado';
 }
-
-console.log(ligarDesligar('desligado'))
+// console.log(ligarDesligar('desligado'))
 // 2 - Crie a função circleArea, que calcule a área de um círculo.
 
-
+function circleArea(raio) {
+  if (typeof raio !== 'number') return 'O parâmetro radius deve ser um número';
+  return `Essa é a área do círculo: ${((raio * raio) * 3.14)}`;
+}
+// console.log(circleArea(5));
 // 3 - Crie a função longestWord, que receba uma frase como parâmetro e retorne a maior palavra da frase.
 
 
